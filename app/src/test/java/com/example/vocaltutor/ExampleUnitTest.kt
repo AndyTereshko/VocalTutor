444e6071186ab4de18b120bc61d0b763795a1519
Andriy Tereshko <andrey.tereshko@gmail.com>
2020-08-06 07:26:52 +0300

    Initial commit

---
+++ app/src/test/java/com/example/vocaltutor/ExampleUnitTest.kt
@@ -0,0 +1,15 @@
+package com.example.vocaltutor
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
